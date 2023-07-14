cbf42b669d1924ba9d8397bd9d47670f83f7b478
Rohit More <101354500+rohitmore07@users.noreply.github.com>
2023-07-14 22:10:07 +0530

    Add files via upload

---
+++ Youtube-Clone/app.js
@@ -0,0 +1,59 @@
+const videoCardContainer = document.querySelector('.video-container');
+
+let api_key= `AIzaSyCHLuRQfVFrSrGKqz0CnTgdZVNj88jc07c`;
+let video_http= `https://www.googleapis.com/youtube/v3/videos?`;
+let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
+fetch(video_http + new URLSearchParams({
+    key: api_key,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 50,
+    regionCode: 'IN' 
+}))
+.then(res => res.json())
+.then(data => {
+    data.items.forEach(item => {        
+        getChannelIcon(item);
+    })
+})
+.catch(err => console.log(err));
+
+const getChannelIcon = (video_data) => {
+    fetch(channel_http + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    }))
+    .then(res => res.json())
+    .then(data => {
+        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+}
+
+const makeVideoCard = (data) => {
+    videoCardContainer.innerHTML += `
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <p class="title">${data.snippet.title}</p>
+                <p class="channel-info">${data.snippet.channelTitle}</p>
+                <p class="channel-info">62.3K views • 3 hours ago</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+    if(searchInput.value.length){
+        location.href = searchLink + searchInput.value;
+    }
+})
